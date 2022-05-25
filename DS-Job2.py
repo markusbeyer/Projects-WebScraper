5b8eda0d53d9e4be5bb8d4f196f9af761600e832
Markus Beyer <markus.beyer10969@web.de>
2022-05-25 16:37:51 +0200

    Created Code

---
+++ DS-Job2.py
@@ -0,0 +1,9 @@
+import requests #not necessary as html file is downloaded
+import xlsxwriter #to write data to xlsx file
+import re, time
+from   bs4 import BeautifulSoup #for scraping all the data
+
+#Scrapes names and emails addresses from HTML site I dont know the URL link of anymore
+#and outputs it into a xsxx file in an ordered manner
+
+
